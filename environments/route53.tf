b26e0dfb6ccc61f906e0cdd73f77806b7e9e6a03
shmador <dorattar4@gmail.com>
2025-05-24 15:07:06 +0300

    Route53 setup for creating website records

---
+++ environments/route53.tf
@@ -0,0 +1,23 @@
+resource "aws_route53_record" "apex" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = var.mydomain
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = var.mydomain_www
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
