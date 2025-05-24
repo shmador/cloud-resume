49c099bef0870550d9a0602a139192433f83266f
shmador <dorattar4@gmail.com>
2025-05-24 15:03:38 +0300

    tfstate s3 bucket setup

---
+++ bootstrap/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "il-central-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "dor-tfstate-bucket" 
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
